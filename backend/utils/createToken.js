7f254ef53987e917d8d68527956041f26515bec8
suhail1malik <suhailmalik7643@gmail.com>
2026-05-02 17:16:22 +0530

    auth issue found

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,8 +6,8 @@ const generateToken = (res, userId) => {
 //set jwt as an HTTTp only cookie 
 res.cookie('jwt', token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV !== 'development',
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'none',
   maxAge: 30 * 24 * 60 * 60 * 1000
 });
   return token;
